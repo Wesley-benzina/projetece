3c9fcfd1f73735bd62a7d1566c834127b423160a
Wesley Benzina <wesley.benzina@edu.ecetech.fr>
2020-08-15 20:21:54 +0200

    Initial commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -24,4 +24,60 @@ class MainController extends AbstractController
     {
         return $this->render('main/main.html.twig');
     }
+
+    /**
+     * @Route("/clients", name="clients")
+     */
+    public function clients()
+    {
+        return $this->render('main/clients.html.twig',[
+                'current_menu' => 'clients'
+            ]
+        );
+    }
+
+    /**
+     * @Route("/edit_user", name="edit_user")
+     */
+    public function edit_user()
+    {
+        return $this->render('main/edit_user.html.twig',[
+                'current_menu' => 'clients'
+            ]
+        );
+    }
+
+    /**
+     * @Route("/projets", name="projets")
+     */
+    public function projects ()
+    {
+        return $this->render('main/projects.html.twig',[
+                'current_menu' => 'projects'
+            ]
+        );
+    }
+
+    /**
+     * @Route("/devisetfacture", name="devisetfacture")
+     */
+    public function devisetfacture ()
+    {
+        return $this->render('main/devisetfacture.html.twig',[
+                'current_menu' => 'devisetfacture'
+            ]
+        );
+    }
+
+    /**
+     * @Route("/taches", name="taches")
+     */
+    public function taches ()
+    {
+        return $this->render('main/taches.html.twig',[
+                'current_menu' => 'taches'
+            ]
+        );
+    }
+
 }
